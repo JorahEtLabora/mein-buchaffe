e5e433356da833bf55d57b969c3a3b08c43226f9
JorahEtLabora <94102169+JorahEtLabora@users.noreply.github.com>
2023-07-27 15:06:07 +0200

    3 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookListComponent } from './book-list/book-list.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, BookListComponent],
   imports: [BrowserModule, AppRoutingModule],
 
   providers: [],
